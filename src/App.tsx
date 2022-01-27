7b8e9a96b904ded7b91d8c77f7ba67631368622e
upship-kim <tkdqook@gmail.com>
2022-01-28 00:54:57 +0900

    update : rename Contentbox -> Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import Layout from './components/atoms/Layout';
 import Router from './routes';
-// *이메일(중복확인- 백엔드), * 비밀번호, * 비밀번호 확인(유효성 검사-프론트),
-//     이름, 성별, 핸드폰번호, 생년월일
+
 function App() {
     return (
         <Layout>
