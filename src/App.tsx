f9967cd7f568cadd2580558703be48d3c42a8819
upship-kim <tkdqook@gmail.com>
2022-01-27 14:24:01 +0900

    toyChat

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
 
 function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-                    Learn React
-                </a>
-            </header>
-        </div>
-    );
+    return <Container>합의금은 두둑히</Container>;
 }
 
 export default App;
+
+const Container = styled.div`
+    display: flex;
+    flex: 1;
+    height: 100vh;
+    justify-content: center;
+    align-items: center;
+`;
